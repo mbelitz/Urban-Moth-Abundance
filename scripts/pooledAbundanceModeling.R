0a694da3910d1b77dbced023c8cc93baa4d773ab
mbelitz <mbelitz@ufl.edu>
2024-01-05 12:31:28 -0500

    properly group data

--- scripts/pooledAbundanceModeling.R
+++ scripts/pooledAbundanceModeling.R
@@ -19,6 +19,15 @@ adult_df <- read.csv("data/rawMeasurements/surveyDateSheet.csv") %>%
   mutate(Date = as_date(mdy(eventDate))) %>% 
   mutate(Site = stringr::str_to_title(Site))
 
+abundanceDF <- adult_df %>% 
+  group_by(Date, Site) %>% 
+  summarise(macroMoths = sum(macroMoths),
+            microMoths = sum(microMoths))
+
+sampleDates <- dplyr::distinct(adult_df, Date, doy2, year, doy)
+
+adult_df <- left_join(abundanceDF, sampleDates)
+
 ## create models for each cumulative measure
 # combine dataset with urbanization values
 urb <- read.csv("data/data_products/urbanization_gradient.csv")
@@ -40,33 +49,62 @@ adult_df <- adult_df%>%
   left_join(lunarIllumination, by = c("Date" = "Date")) %>% 
   mutate(rel_temp = (mean_temp - 1.02)*-1) 
 
-adult_df <- adult_df %>% 
+meanDev <- mean(adult_df$Dev_1)
+sdDev <- sd(adult_df$Dev_1)
+
+adult_df <- ungroup(adult_df) %>% 
   mutate(rel_temp = scale(rel_temp),
          tmin = scale(tmin),
          prcp = scale(prcp),
          Dev_1 = scale(Dev_1),
          lunarIllumination = scale(lunarIllumination))
 
+# get priors
+get_prior(macroMoths ~ Dev_1  + lunarIllumination + prcp + tmin +
+            (1|Site), data = adult_df, family = zero_inflated_negbinomial())
+
+slopePrior <- prior(normal(0,10), class = b)
 # make my one, well thought-out model
-macro_dev <- brm(formula = bf( macroMoths ~ Dev_1  + rel_temp + lunarIllumination + prcp + tmin +
+set.seed(2963)
+macro_dev <- brm(formula = bf(macroMoths ~ Dev_1  + lunarIllumination + prcp + tmin +
                                     (1|Site),
                                   zi ~ Dev_1 + lunarIllumination + prcp + tmin + (1|Site)),
                             data = adult_df,
                             family = zero_inflated_negbinomial(),
                             chains = 4, iter = 2400, warmup = 1000,
-                            control = list(adapt_delta = 0.98),
+                            control = list(adapt_delta = 0.99),
                             cores = 4, seed = 1234, 
                             threads = threading(2),
                             backend = "cmdstanr", 
-  )
+                 prior = slopePrior)
 
 
 
 # examine model assumptions
 plot(macro_dev)
-pp_check(macro_dev)
+pp_check(macro_dev, ndraws = 100)
 pp_check(macro_dev, type = "stat", stat = "mean")
 
+# check for spatial autocorrelation
+resids <- residuals(macro_dev)
+
+resids_df <- adult_df %>% 
+  mutate(resids = resids[,1]) %>% 
+  group_by(Site) %>% 
+  summarise(meanResid = mean(resids)) %>% 
+  ungroup()
+
+library(sf)
+coordsDF <- read.csv("data/data_products/siteCoordinates.csv")
+coordsDF_m <- st_as_sf(coordsDF, coords = c("long", "lat"), crs = "WGS84") %>% 
+  st_transform(crs = "+proj=laea +lon_0=-78.7495368 +lat_0=32.7548488 +datum=WGS84 +units=m +no_defs")
+x <- st_coordinates(coordsDF_m)[,1]
+y <- st_coordinates(coordsDF_m)[,2]
+  
+resids_df <- left_join(resids_df, coordsDF)
+library(ncf)
+spAuCoTest <- ncf::correlog(x = x, y, resids_df$meanResid, latlon = F, increment = 100) 
+plot(spAuCoTest) # no signal of spatial autocorrelation in residuals
 
 summary(macro_dev, prob = 0.89)
 conditional_effects(x = macro_dev, effects = "prcp", prob = 0.89, dpar = "zi")
@@ -77,14 +115,16 @@ macro_sum <- summary(macro_dev, prob = 0.89)$fixed %>%
 ce <- conditional_effects(x = macro_dev, effects = "Dev_1", prob = 0.89)
 ce_df <- ce$Dev_1
 
+# plot on original urb development scale 
 macro_plot <- ggplot() +
-  geom_jitter(adult_df, mapping = aes(x = Dev_1, y = macroMoths), alpha = 0.5) +
-  geom_line(ce_df, mapping = aes(x = Dev_1, y = estimate__)) +
-  geom_ribbon(ce_df, mapping = aes(x = Dev_1, ymax = upper__, ymin = lower__), alpha = 0.3) +
+  geom_jitter(adult_df, mapping = aes(x = meanDev + (sdDev*Dev_1), y = macroMoths), alpha = 0.5) +
+  geom_line(ce_df, mapping = aes(x = meanDev + (sdDev*Dev_1), y = estimate__)) +
+  geom_ribbon(ce_df, mapping = aes(x = meanDev + (sdDev*Dev_1), ymax = upper__, ymin = lower__), alpha = 0.3) +
   labs(x = "Urban development", y = "Total abundance") +
   ggtitle("Macro moths") +
   theme_bw() +
-  theme(plot.title = element_text(hjust = 0.5, size = 13))
+  theme(plot.title = element_text(hjust = 0.5, size = 13),
+        axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 
 macro_plot
 
@@ -92,7 +132,7 @@ macro_plot
 adult_df_noBaca <- adult_df %>% 
   filter(Site != "Baca")
 # make my one, well thought-out model
-macro_dev_noBaca <- brm(formula = bf( macroMoths ~ Dev_1  + rel_temp + 
+macro_dev_noBaca <- brm(formula = bf( macroMoths ~ Dev_1  + 
                                         lunarIllumination + prcp + tmin +
                                  (1|Site),
                                zi ~ Dev_1 + lunarIllumination + prcp + tmin + (1|Site)),
@@ -103,6 +143,7 @@ macro_dev_noBaca <- brm(formula = bf( macroMoths ~ Dev_1  + rel_temp +
                  cores = 4, seed = 1234, 
                  threads = threading(2),
                  backend = "cmdstanr", 
+                 prior = slopePrior
 )
 
 # examine model assumptions
@@ -118,7 +159,8 @@ macro_sum_noBaca <- summary(macro_dev_noBaca, prob = 0.89)$fixed %>%
 #######################MICROMOTHS###############################################
 ################################################################################
 # make my one, well thought-out model
-micro_dev <- brm(formula = bf( microMoths ~ Dev_1  + rel_temp + 
+set.seed(42)
+micro_dev <- brm(formula = bf( microMoths ~ Dev_1  +
                                  lunarIllumination + prcp + tmin +
                                  (1|Site),
                                zi ~ Dev_1 + lunarIllumination + prcp + tmin + (1|Site)),
@@ -129,13 +171,33 @@ micro_dev <- brm(formula = bf( microMoths ~ Dev_1  + rel_temp +
                  cores = 4, seed = 1234, 
                  threads = threading(2),
                  backend = "cmdstanr", 
+                 prior = slopePrior
 )
 
 # examine model assumptions
 plot(micro_dev)
-pp_check(micro_dev)
+pp_check(micro_dev,ndraws = 100)
 pp_check(micro_dev, type = "stat", stat = "mean")
 
+# check for spatial autocorrelation
+resids <- residuals(micro_dev)
+
+resids_df <- adult_df %>% 
+  mutate(resids = resids[,1]) %>% 
+  group_by(Site) %>% 
+  summarise(meanResid = mean(resids)) %>% 
+  ungroup()
+
+resids_df <- left_join(resids_df, coordsDF)
+
+spAuCoTest <- ncf::correlog(x = x, y, resids_df$meanResid, latlon = F, increment = 100) 
+plot(spAuCoTest) # no signal of spatial autocorrelation in residuals
+
+ggplot(resids_df, mapping = aes(x = long, y = lat, color = meanResid)) +
+  geom_point(size = 2) +
+  scale_color_gradient2() 
+
+# results
 summary(micro_dev, prob = 0.89)
 micro_sum <- summary(micro_dev, prob = 0.89)$fixed %>% 
   tibble::rownames_to_column()
@@ -144,18 +206,20 @@ ce_micro <- conditional_effects(x = micro_dev, effects = "Dev_1", prob = 0.89)
 ce_micro_df <- ce_micro$Dev_1
 
 micro_plot <- ggplot() +
-  geom_jitter(adult_df, mapping = aes(x = Dev_1, y = microMoths), alpha = 0.5) +
-  geom_line(ce_micro_df, mapping = aes(x = Dev_1, y = estimate__)) +
-  geom_ribbon(ce_micro_df, mapping = aes(x = Dev_1, ymax = upper__, ymin = lower__), alpha = 0.3) +
+  geom_jitter(adult_df, mapping = aes(x = meanDev + (sdDev*Dev_1), y = microMoths), alpha = 0.5) +
+  geom_line(ce_micro_df, mapping = aes(x = meanDev + (sdDev*Dev_1), y = estimate__)) +
+  geom_ribbon(ce_micro_df, mapping = aes(x = meanDev + (sdDev*Dev_1), ymax = upper__, ymin = lower__), alpha = 0.3) +
   labs(x = "Urban development", y = "Total abundance") +
   ggtitle("Micro moths") +
   theme_bw() +
-  theme(plot.title = element_text(hjust = 0.5, size = 13))
+  theme(plot.title = element_text(hjust = 0.5, size = 13),
+        axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 
 micro_plot
 
 # micro model no baca
-micro_dev_noBaca <- brm(formula = bf(microMoths ~ Dev_1  + rel_temp + 
+set.seed(21)
+micro_dev_noBaca <- brm(formula = bf(microMoths ~ Dev_1  +  
                                        lunarIllumination + prcp + tmin +
                                  (1|Site),
                                zi ~ Dev_1 + lunarIllumination + prcp + tmin + (1|Site)),
@@ -166,6 +230,7 @@ micro_dev_noBaca <- brm(formula = bf(microMoths ~ Dev_1  + rel_temp +
                  cores = 4, seed = 1234, 
                  threads = threading(2),
                  backend = "cmdstanr", 
+                 prior = slopePrior
 )
 
 # examine model assumptions
@@ -264,6 +329,9 @@ frass_df_perSite <- ungroup(frass_df_perSite) %>%
   left_join(lunarDates_df) %>% 
   mutate(rel_temp = (mean_temp - 1.02)*-1)
 
+meanDev <- mean(frass_df_perSite$Dev_1)
+sdDev <- sd(frass_df_perSite$Dev_1)
+
 frass_df_perSite <- frass_df_perSite %>% 
   mutate(rel_temp = scale(rel_temp),
          mean_tmin = scale(mean_tmin),
@@ -273,7 +341,8 @@ frass_df_perSite <- frass_df_perSite %>%
   )
 
 
-frass_dev <- brm(formula = bf(MeanMassPerDay ~ Dev_1  + rel_temp +
+set.seed(789)
+frass_dev <- brm(formula = bf(MeanMassPerDay ~ Dev_1  +
                                 meanLunarIllumination + mean_tmin + mean_prcp +
                                 (1|Site)),
                  data = frass_df_perSite,
@@ -283,11 +352,12 @@ frass_dev <- brm(formula = bf(MeanMassPerDay ~ Dev_1  + rel_temp +
                  cores = 4, seed = 1234, 
                  threads = threading(2),
                  backend = "cmdstanr", 
+                 prior = slopePrior
 )
 
 # examine model assumptions
 plot(frass_dev)
-pp_check(frass_dev, ndraws = 50)
+pp_check(frass_dev, ndraws = 100)
 pp_check(frass_dev, type = "stat", stat = "mean")
 
 summary(frass_dev, prob = 0.89)
@@ -299,19 +369,21 @@ ce_frass <- conditional_effects(x = frass_dev, effects = "Dev_1", prob = 0.89)
 ce_frass_df <- ce_frass$Dev_1
 
 frass_plot <- ggplot() +
-  geom_jitter(frass_df_perSite, mapping = aes(x = Dev_1, y = MeanMassPerDay), alpha = 0.5) +
-  geom_line(ce_frass_df, mapping = aes(x = Dev_1, y = estimate__)) +
-  geom_ribbon(ce_frass_df, mapping = aes(x = Dev_1, ymax = upper__, ymin = lower__), alpha = 0.3) +
+  geom_jitter(frass_df_perSite, mapping = aes(x = meanDev + (sdDev*Dev_1), y = MeanMassPerDay), alpha = 0.5) +
+  geom_line(ce_frass_df, mapping = aes(x = meanDev + (sdDev*Dev_1), y = estimate__)) +
+  geom_ribbon(ce_frass_df, mapping = aes(x = meanDev + (sdDev*Dev_1), ymax = upper__, ymin = lower__), alpha = 0.3) +
   labs(x = "Urban development", y = "Mean frass mass per day \n log(x + 0.001)") +
   ggtitle("Caterpillars") +
   theme_bw() +
-  theme(plot.title = element_text(hjust = 0.5, size = 13))
+  theme(plot.title = element_text(hjust = 0.5, size = 13),
+        axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 
 frass_plot
 
 # frass model no baca
 frass_df_perSite_noBaca <- filter(frass_df_perSite, Site != "Baca")
-frass_dev_noBaca <- brm(formula = bf(MeanMassPerDay ~ Dev_1  + rel_temp +
+set.seed(987)
+frass_dev_noBaca <- brm(formula = bf(MeanMassPerDay ~ Dev_1  +
                                        meanLunarIllumination + mean_tmin + mean_prcp +
                                 (1|Site)),
                  data = frass_df_perSite_noBaca,
@@ -321,6 +393,7 @@ frass_dev_noBaca <- brm(formula = bf(MeanMassPerDay ~ Dev_1  + rel_temp +
                  cores = 4, seed = 1234, 
                  threads = threading(2),
                  backend = "cmdstanr", 
+                 prior = slopePrior
 )
 
 # examine model assumptions
